3f89dadf0ed099b7c7b0e315ed5557310cb5a0f6
Vijaya Namagiri <Vijaya.Namagiri@neudesic.com>
2023-04-19 22:11:11 +0530

    code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        public static void Main()
+        {
+            double x;
+            Console.Write("Enter the number for which u want a Squareroot : ");
+            x = int.Parse(Console.ReadLine());
+
+
+            Console.Write(Math.Sqrt(x));
+        }
+    }
+}
